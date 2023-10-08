57ab5e386a573799d167be76d35d026a7c900b32
davelin18yufan <davelin30630@gmail.com>
2023-10-08 12:25:43 +0800

    implement provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter, Space_Grotesk } from "next/font/google";
 import type { Metadata } from "next";
 
 import "./globals.css";
+import { ThemeProvider } from "@/context/ThemeProvider";
 
 // setting font styles with function provided by next/google
 const inter = Inter({
@@ -35,18 +36,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider
-    appearance={{
-      elements: {
-        formButtonPrimary: "primary-gradient",
-        footerActionLink: "primary-text-gradient hover:text-primary-500"
-      }
-    }}>
-      <html lang="en">
-        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: "primary-gradient",
+              footerActionLink: "primary-text-gradient hover:text-primary-500",
+            },
+          }}
+        >
+          <ThemeProvider>{children}</ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
