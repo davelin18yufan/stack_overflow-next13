e0953149cf90b353b9c3a737e66749b7bbb032bb
davelin18yufan <davelin30630@gmail.com>
2023-10-16 22:29:17 +0800

    add Form and questionSchema

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,8 +1,15 @@
+import Question from '@/components/forms/Question'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>Ask Question</div>
+    <div className="h1-bold text-dark100-light900"> 
+      <h1>Ask a Question</h1>
+      
+      <div className="mt-9">
+        <Question/>
+      </div>
+    </div>
   )
 }
 
