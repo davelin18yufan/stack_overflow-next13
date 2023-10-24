f9624690dd63e44bb1e63bc53d54b33a695e4341
Dave.lin <dave.lin@iwplay.com.tw>
2023-10-24 14:54:29 +0800

    add webhook secret and api webhook to publicRoute

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -6,7 +6,7 @@ import { NextResponse } from "next/server"
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
+  const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
@@ -52,6 +52,8 @@ export async function POST(req: Request) {
 
   // Get the ID and type
   const eventType = evt.type
+
+  console.log(eventType)
 
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, username, first_name, last_name } =
