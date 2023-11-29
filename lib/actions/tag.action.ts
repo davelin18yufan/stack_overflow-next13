655819c29463358d5d1a7373404d604d6e91ef34
davelin18yufan <davelin30630@gmail.com>
2023-11-29 22:07:57 +0800

    [refactor] modify getTopInteractionTags

--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -24,37 +24,20 @@ export async function getTopInteractiveTags(
 
     if (!user) throw Error("user not found")
 
-    //* find interactions for the user and grown tags...
-    // get the corresponding interaction
-    const userInteraction = await Interaction.find({ user: user._id })
-      .populate("tags")
-      .exec()
-    // extract tags
-    const userTags = userInteraction.reduce((tags, interaction) => {
-      if (interaction.tags) {
-        tags = tags.concat(interaction.tags)
-      }
-      return tags
-    }, [])
-    // remove duplicate tag ID
-    const distinctUserTagIds = [
-      // @ts-ignore
-      ...new Set(userTags.map((tag) => tag._id)),
-    ]
+    // find interactions for the user and group tags...
+    const tagCountMap = await Interaction.aggregate([
+      { $match: { user: user._id, tags: { $exists: true, $ne: [] } } },
+      { $unwind: "$tags" },
+      { $group: { _id: "$tags", count: { $sum: 1 } } },
+      { $sort: { count: -1 } },
+      { $limit: limit },
+    ])
 
-    // depends on the user's most interactive tag -> query to question
-    const query: FilterQuery<typeof Tag> = {
-      $and: [
-        { _id: { $in: distinctUserTagIds } }, 
-      ],
-    }
+    const topTags = tagCountMap.map((tag) => tag._id)
+    // Find the tag
+    const topTagDocument = await Tag.find({ _id: { $in: topTags } })
 
-    const interactiveTags = await Tag.find(query).limit(3)
-    return interactiveTags.map(item => ({
-      _id: item._id,
-      name: item.name
-     }))
-
+    return topTagDocument
   } catch (error) {
     console.log(error)
     throw error
@@ -140,7 +123,7 @@ export async function getQuestionByTagId(params: GetQuestionsByTagIdParams) {
 
     const questions = tag.questions
 
-    const hasNextPage = questions.length > pageSize 
+    const hasNextPage = questions.length > pageSize
 
     return { tagTitle: tag.name, questions, hasNextPage }
   } catch (error) {
