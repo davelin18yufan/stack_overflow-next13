a1152ae0834f035c2c17e2c92d47563545941ca6
davelin18yufan <davelin30630@gmail.com>
2024-01-09 10:52:17 +0800

    [feat] add GlobalResult and GlobalFilter test

--- components/shared/search/__tests__/GlobalFilter.test.tsx
+++ components/shared/search/__tests__/GlobalFilter.test.tsx
@@ -1,18 +1,17 @@
-import { render, screen} from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { userEvent } from "@testing-library/user-event"
-import { useRouter } from "next/navigation"
+import { useRouter, useSearchParams } from "next/navigation"
 import GlobalFilter from "../GlobalFilter"
-import GlobalResult from "../GlobalResult"
-import GlobalSearch from "../GlobalSearch"
 
 const mockSearchParams = new URLSearchParams()
+
 const mockReplace = jest.fn()
 jest.mock("next/navigation", () => ({
   usePathname: () => "/mocked-path",
-  useSearchParams: () => mockSearchParams,
+  useSearchParams: jest.fn(),
   useRouter: () => ({
-    replace: mockReplace
-  })
+    replace: mockReplace,
+  }),
 }))
 
 afterEach(() => {
@@ -20,36 +19,41 @@ afterEach(() => {
 })
 
 describe("GlobalFilter", () => {
-  const user = userEvent.setup()
-  it("Should render correct filters", async () => {
-    render( <GlobalFilter/>)
+  const user = userEvent.setup();
 
-    expect(screen.getByText('Question')).toBeInTheDocument()
+  (useSearchParams as jest.Mock).mockReturnValue(mockSearchParams)
+  
+  it("Should render correct filters", async () => {
+    render(<GlobalFilter />)
+
+    expect(screen.getByText("Question")).toBeInTheDocument()
     expect(screen.getByText("User")).toBeInTheDocument()
   })
 
   it("Should append correct style after clicked", async () => {
-    render(<GlobalFilter/>)
+    render(<GlobalFilter />)
 
     const btn = screen.getByRole("button", { name: "User" })
 
     // activate
     await user.click(btn)
-    expect(btn).toHaveClass('bg-primary-500')
+    expect(btn).toHaveClass("bg-primary-500")
 
     // cancel
     await user.click(btn)
-    expect(btn).not.toHaveClass('bg-primary-500')
-    
+    expect(btn).not.toHaveClass("bg-primary-500")
   })
 
   it("Should add correct param to url after clicked", async () => {
-    render(<GlobalFilter/>)
+    render(<GlobalFilter />)
 
     const btn = screen.getByRole("button", { name: "Question" })
-    
+
     await user.click(btn)
 
-    expect(useRouter().replace).toHaveBeenCalledWith('/mocked-path?type=question')
+    expect(useRouter().replace).toHaveBeenCalledWith(
+      "/mocked-path?type=question"
+    )
   })
 })
+
