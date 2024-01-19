b6835b8120db28beacf0e1e2ef0d20f13c58e8b7
davelin18yufan <davelin30630@gmail.com>
2024-01-19 15:13:41 +0800

    [feat] add ParseHTML test

---
+++ components/shared/__tests__/ParseHTML.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen} from "@testing-library/react"
+import ParseHTML from "../ParseHTML"
+
+describe("ParseHTML", () => {
+  it("Should render correct data", () => {
+    render(<ParseHTML data="<h1>Testing</h1>"/>)
+
+    expect(screen.getByText('Testing').tagName).toBe('h1')
+  })
+})
